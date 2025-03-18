d579ccb03b981c3f1c0999f6a394409fa4dd46e2
SHANMUGAM-K-S <magizhchisk@gmail.com>
2025-03-18 15:30:48 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+//  let age: number=20;
+//  if(age<50)
+//     age+=10;
+// console.log(age);
+
+
+// let sales=8939_614_323;
+// let course="typescript";
+// let is_published=true ;
+
+// // if we doesn't declare any value it  is "any"  avoid "any" data type
+
+// // let type;
+// // type=1;
+// // type ='sha';
+
+// // in this function also without specifying the data type it is must be a "any" data type  ...if you want to use use ":any" 
+// function render(document :any){
+//     console.log(document);
+    
+// }
+
+
+// let numbers=[1,2,3];
+// let array: number[]=[];
+// array[0]=1;
+// array[1]=2;
+// array[2]='5'
+
+
+
+let numbers: number[]=[];
+numbers.forEach(n=>n.valueOf)
